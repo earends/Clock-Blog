62b5543c083851d115ed5170adc85fd3ccac1976
Evan Arends <evan.arends@avanade.com>
2019-02-12 20:16:10 -0800

    delete and update event works

--- src/app/update-event/update-event.component.ts
+++ src/app/update-event/update-event.component.ts
@@ -1,15 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import {Event} from '../event';
+import {EventService} from '../event.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-update-event',
   templateUrl: './update-event.component.html',
   styleUrls: ['./update-event.component.css']
 })
-export class UpdateEventComponent implements OnInit {
+export class UpdateEventComponent implements AfterViewInit {
 
-  constructor() { }
+  constructor(private eventService:EventService,private router:Router) { }
+  events:Event [];
+  isEditEvent:boolean = false;
+  selectedEvent: Event;
 
-  ngOnInit() {
+  ngAfterViewInit() {
+    this.getEvents();
   }
 
+  getEvents() {
+    this.eventService.GetEvents()
+    .subscribe(events => this.events = events);
+
+  }
+
+  onSelectEvent(event:Event) {
+    this.selectedEvent = event;
+    this.isEditEvent = true;
+  }
+
+  onSubmit() {
+    this.eventService.updateEvent(this.selectedEvent)
+    .subscribe(event => { 
+      this.router.navigate([`/Events`])
+    });
+  }
+
+  onDelete() {
+    this.eventService.deleteEvent(this.selectedEvent.id)
+    .subscribe(event => { 
+      this.router.navigate([`/Events`])
+    });
+  }
 }
